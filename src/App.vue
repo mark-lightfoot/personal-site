fee5c5e2fc3f8f5185fddb2c8044543d0408767e
Bltt <marklightfoot96@gmail.com>
2022-04-07 14:26:10 +0000

    Big Bang

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<script setup>
+import Me from './components/Me.vue'
+import HomeList from './components/HomeList.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <Me />
+    </div>
+  </header>
+
+  <main>
+    <HomeList />
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+ul {
+  list-style-type: square;
+  margin: 2px;
+  margin-left: 1.5rem;
+  padding: 0;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: rgb(223, 120, 24);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(36, 82%, 26%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
---
+++ src/components/HomeList.vue
@@ -0,0 +1,73 @@
+<script setup>
+import HomeListItem from './HomeListItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <HomeListItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Career</template>
+
+    Senior Platform Engineer @ <a href="https://cts.co">CTS</a> (2018-2022)
+  </HomeListItem>
+
+  <HomeListItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Learning</template>
+
+    BSc in Business Information Technology from <a href="https://aber.ac.uk">Aberystwyth University</a>.
+
+    <br />
+    GCP Professional Architect
+    <br />
+    GCP Professional DevOps Engineer
+    <br />
+    Certified Kubernetes Administrator
+    <br />
+    Certified Kubernetes Security Specialist
+  </HomeListItem>
+
+  <HomeListItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Contacts</template>
+
+    <a target="_blank" href="https://twitter.com/markgingerninja/">Twitter</a><br />
+    <a target="_blank" href="https://linkedin.com/in/mark-l-0a8917116//">LinkedIn</a><br />
+    <a target="_blank" href="https://github.com/mark-lightfoot/">Github</a><br />
+    <a target="_blank" href="mailto:mark@gingerninja.co">Email</a><br />
+  </HomeListItem>
+
+  <HomeListItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Interests</template>
+
+    <ul>
+      <li>DevOps</li>
+      <li>SRE</li>
+      <li>CI/CD</li>
+      <li>Mentoring/Helping/Inspiring</li>
+      <li>Aviation, the RAF and shooting</li>
+    </ul>
+  </HomeListItem>
+
+  <HomeListItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>WIP!!</template>
+
+    The site is WIP. If you know Vue, you can probably see I havent even changed from the bootstrap. Im still learning.
+  </HomeListItem>
+</template>
